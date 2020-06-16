3824dcfa49a20626775c35fc296e87eebdcec24a
carolrss <65054120+carolrss@users.noreply.github.com>
2020-06-16 11:16:06 -0300

    Add files via upload

---
+++ Prova/Prova/Prova/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Prova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstbResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(77, 141);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(163, 90);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lstbResultado
+            // 
+            this.lstbResultado.FormattingEnabled = true;
+            this.lstbResultado.ItemHeight = 16;
+            this.lstbResultado.Location = new System.Drawing.Point(342, 19);
+            this.lstbResultado.Name = "lstbResultado";
+            this.lstbResultado.Size = new System.Drawing.Size(419, 404);
+            this.lstbResultado.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Avaliação Pratica LP2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstbResultado;
+    }
+}
+
---
+++ Prova/Prova/Prova/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double[,] VendasMeses = new double[6, 4];
+            double[] TotalCadaMes = new double[6];
+            double TotalGeral = 0;
+            int m, s = 0;
+            string valores = "";
+
+            for ( m = 0; m < 6; m++ )
+            {
+                for ( s = 0; s < 4; s++ )
+                {
+                    valores = Interaction.InputBox("Mês: " + (m + 1) + "\n" + "Digite o valor de vendas da semana " + (s + 1), "Valor das Vendas na semana");
+                    if (double.TryParse(valores,out VendasMeses[m,s]))
+                    {
+                        
+                        lstbResultado.Items.Add("Total do mês: " + (m + 1) + " Semana: " + (s + 1) + " R$" +VendasMeses[m, s].ToString("N2")+"\n");
+                        TotalCadaMes[m] += VendasMeses[m, s];
+                       
+
+                    }
+                    else
+                    {
+                        MessageBox.Show(" Erro: Valor Inválido");
+                        s--;
+                    }
+                }
+                
+                lstbResultado.Items.Add(">> Total Mês:R$" + TotalCadaMes[m].ToString("N2")+"\n");
+                lstbResultado.Items.Add("--------------"+"\n");
+                TotalGeral += TotalCadaMes[m];
+               
+
+
+            }
+            
+            lstbResultado.Items.Add(">> Total Geral:R$" + TotalGeral.ToString("N2"));
+             
+
+        }
+    }
+}
